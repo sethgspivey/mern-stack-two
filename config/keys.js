c9d65db7d5d19b0ec7b08f0fc8a68fcf1ad89018
Seth Spivey <seth@fourcolumns.net>
2019-08-01 09:01:42 -0500

    Second Commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb+srv://Scriptco_dataAdmin:DFIhJ9jardmBvQNn@scriptco-data-8499i.mongodb.net/Scriptco_Dataset?retryWrites=true&w=majority", secretOrKey: "secret"
+};
