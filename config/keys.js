f471da67cde5f318ca76a2c188e5bad1bf4caf56
Seth Spivey <seth@fourcolumns.net>
2019-08-01 09:17:15 -0500

    Second Commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
-  mongoURI: "mongodb+srv://Scriptco_dataAdmin:DFIhJ9jardmBvQNn@scriptco-data-8499i.mongodb.net/Scriptco_Dataset?retryWrites=true&w=majority", secretOrKey: "secret"
+  mongoURI: "mongodb+srv://process.env.MONOGDB_USER:process.env.MONGODB_PASS@scriptco-data-8499i.mongodb.net/Scriptco_Dataset?retryWrites=true&w=majority", secretOrKey: "secret"
 };
